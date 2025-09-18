f99ee9433eba8793b0a4c523228ff6e872aebe74
DHorner14 <194024-7@iberoleon.edu.mx>
2025-09-18 11:56:04 -0600

    Presentacion Diego Horner

--- clases/presentacion.py
+++ clases/presentacion.py
@@ -4,5 +4,8 @@ class Presentacion:
 
     def presentarAlejandroMontes(self):
         print("Hola mi nombre es Alejandro Montes")
+    
+    def presentarDiegoHorner(self):
+        print("Hola mi nombre es Diego Horner")
 
     #Crear aqui el metodo para tu presentacion
