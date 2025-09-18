a390b2239d19a9f1bda1ee7844d4f92a519bc838
Alejandro Montes Moreno <alejandromontesmoreno@gmail.com>
2025-09-18 07:52:25 -0600

    Versión 1 - Código base del proyecto

---
+++ main.py
@@ -0,0 +1,12 @@
+from clases.presentacion import Presentacion
+
+def main():
+    presenta = Presentacion()
+
+    presenta.presentarAlejandroMontes()
+
+    #Realiza aqui la llamada al metodo que te presenta
+
+
+if __name__ == '__main__':
+    main()
