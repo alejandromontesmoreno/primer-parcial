f6456dacded49ceb6b0cd182a29c9a5db50fd203
AlexGove <193007-a@iberoleon.edu.mx>
2025-09-18 12:31:34 -0600

    Crear el metodo para mi nombre en presentacion.py, crear linea para llamar a la funcion en main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
     presenta = Presentacion()
 
     presenta.presentarAlejandroMontes()
+    presenta.presentarAlexanderGongora()
 
     #Realiza aqui la llamada al metodo que te presenta
 
