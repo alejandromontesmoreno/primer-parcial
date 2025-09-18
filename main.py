1df0314d7cb7f53ccddb1f85979c3c9f032e14b1
Emiliano Mendoza <194604-8@iberoleon.edu.mx>
2025-09-18 14:17:41 -0600

    Modificaciones

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from clases.presentacion import Presentacion
 def main():
     presenta = Presentacion()
 
-    presenta.presentarAlejandroMontes()
+    presenta.presentarEmilianoMendoza()
 
     #Realiza aqui la llamada al metodo que te presenta
 
