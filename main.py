8dd12f1372164acf3e0e3494043b2de7be82636f
Patricio Perez <62504leo@universidaddeleon.edu.mx>
2025-10-06 09:19:54 -0600

    Version 1 agregue mi presentacion

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ def main():
     presenta = Presentacion()
 
     presenta.presentarAlejandroMontes()
+    
+    
 
     #Realiza aqui la llamada al metodo que te presenta
-
+    presenta.presentarTuNombre()
 
 if __name__ == '__main__':
     main()
