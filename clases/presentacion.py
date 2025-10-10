0fd8cbe9abfdcceab9bb3f287b428f3f1721bd91
Sergio Ricardo Oviedo Rodriguez <64406leo@universidaddeleon.edu.mx>
2025-10-09 18:40:58 -0600

    Version sergio

--- clases/presentacion.py
+++ clases/presentacion.py
@@ -6,3 +6,6 @@ class Presentacion:
         print("Hola mi nombre es Alejandro Montes")
 
     #Crear aqui el metodo para tu presentacion
+
+    def presentarSergioOviedo(self):
+        print("Hola mi nombre es Sergio Oviedo")
