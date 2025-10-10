0f83442d83b3b0cd03de5f890204dd724b3b533c
KingCAbraham <charlyccr4@gmail.com>
2025-10-09 21:31:23 -0600

    Se agrego nueva llamada con mi nombre y se corroboro su funcionamiento 09/10/2025

--- clases/presentacion.py
+++ clases/presentacion.py
@@ -6,3 +6,5 @@ class Presentacion:
         print("Hola mi nombre es Alejandro Montes")
 
     #Crear aqui el metodo para tu presentacion
+    def presentarCarlosChavez(self):
+        print("Hola mi nombre es Carlos Chavez")
